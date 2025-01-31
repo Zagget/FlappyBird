379400d9fbd3dee1557f2198fccd1f3792cb36db
Jonathan Romskaug <jonathan.romskaug@gmail.com>
2025-01-31 15:00:24 +0100

    Added sound and CLeaned up hierarchy

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -7,41 +5,56 @@ using UnityEngine.UI;
 public class UIManager : Subject, IObserver
 {
     [SerializeField] Subject gameManagerSubject;
+
+    [Header("Text")]
     [SerializeField] TextMeshProUGUI scoreText;
+
+    [Header("Overlays")]
     [SerializeField] GameObject InGameOverlay;
     [SerializeField] GameObject diedOverlay;
+
+    [Header("Buttons")]
+    [SerializeField] Button retry;
+    [SerializeField] Button menu;
 
     int highScore;
 
     void Start()
     {
-        diedOverlay.SetActive(false);
-        InGameOverlay.SetActive(true);
+        SetOverlay(diedOverlay, false);
+        SetOverlay(InGameOverlay, true);
     }
 
-    public void OnNotify(Events action, int value)
+    public void OnNotify(Events @event, int value)
     {
-        Debug.Log($"UIManager received event: {action} with value: {value}");
-        if (action == Events.PassedPipe)
+        Debug.Log($"UIManager received event: {@event} with value: {value}");
+        if (@event == Events.PassedPipe)
         {
             UpdateScore(value);
         }
 
-        if (action == Events.Die)
+        if (@event == Events.Die)
         {
-
+            SetOverlay(InGameOverlay, true);
         }
 
-        if (action == Events.NewHighScore)
+        if (@event == Events.NewHighScore)
         {
 
         }
     }
+
+
 
     void UpdateScore(int value)
     {
 
         scoreText.text = value.ToString();
+    }
+
+    void SetOverlay(GameObject overlay, bool show)
+    {
+        overlay.SetActive(show);
     }
 
     void OnEnable()
